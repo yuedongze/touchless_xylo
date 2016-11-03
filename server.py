ef8c9c3304778a94f0dc8d1fc9d2ecde772e30a6
Steven Yue <dy85@cornell.edu>
2016-11-03 15:13:27 -0400

    got

--- server.py
+++ server.py
@@ -1,13 +1,41 @@
-from flask import Flask,request,url_for,redirect, url_for, render_template
-import os
+from flask import Flask, request
+import serial
+import subprocess
 from time import sleep
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+class state:
+	ser = 0
+
+@app.route('/', methods=['GET','POST'])
 def pyhandler():
 	content = request.json
-	print content['azim'], content['elev']
+	hpy = content['elev']
+	hpx = content['azim']
+	state.ser.write(str(hpy) + ' ' + str(hpx))
+	print state.ser.readline()
 	return '''content'''
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	
+	port = subprocess.check_output("dmesg | grep 'cdc_acm 1-1.2' | tail -1", shell=True).split(':')
+	if port == ['']:
+		raise Exception("NO DEV")
+	port = '/dev/'+port[2].strip()
+	print port
+	try:
+		state.ser = serial.Serial(port, baudrate=115200, timeout=10)
+	except NameError:
+		raise Exception("ERR")
+	
+	if state.ser.isOpen():
+		state.ser.close()
+	state.ser.open()
+
+	while True:
+		line = state.ser.readline()
+		if 'done initializing' in line:
+			print line
+			break
+	
+	app.run(host='0.0.0.0')
